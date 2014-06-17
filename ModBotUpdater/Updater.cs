f74bd268689ca48fa622e74a104a486a554bb207
Dor <DorCoMaNdO@gmail.com>
2014-06-17 18:46:59 +0300

    Removed unused includes in the updater. Fixed some issues with the updater's project (showing form as classes, giving no access to the designer).

--- ModBotUpdater/Updater.cs
+++ ModBotUpdater/Updater.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Reflection;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ModBotUpdater
